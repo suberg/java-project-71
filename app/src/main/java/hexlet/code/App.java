bf853e21cd83df1ff381a075d23ff0e354de46ff
Anton Yakunov <Yakunov94@gmail.com>
2024-12-15 00:40:21 +0700

    Arguments and options

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -2,6 +2,8 @@ package hexlet.code;
 
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
 
 @Command(name = "gendiff", mixinStandardHelpOptions = true,
         description = "Compares two configuration files and shows a difference.")
@@ -10,6 +12,15 @@ public class App implements Runnable {
         int exitCode = new CommandLine(new App()).execute(args);
         System.exit(exitCode);
     }
+
+    @Parameters(index = "0", description = "path to first file")
+    private String filepath1;
+
+    @Parameters(index = "1", description = "path to second file")
+    private String filepath2;
+
+    @Option(names = {"-f", "--format"}, description = "output format [default: stylish]")
+    private String format;
 
     @Override
     public void run() {
